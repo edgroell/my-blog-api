18f21885a25c539299324c2cc662a7d85ea2041a
Ed Groell <email@edgroell.com>
2025-06-27 12:02:13 +0200

    new increment x1 - (1) added POST function with data validation and new ID generation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,10 +9,53 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
+def validate_post_data(new_post):
+    if (("title" not in new_post or new_post["title"] == "")
+            and ("content" not in new_post or new_post["content"] == "")):
+        return False
+    return True
 
-@app.route('/api/posts', methods=['GET'])
+
+def validate_post_title(new_post):
+    if "title" not in new_post or new_post["title"] == "":
+        return False
+    return True
+
+
+def validate_post_content(new_post):
+    if "content" not in new_post or new_post["content"] == "":
+        return False
+    return True
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'POST':
+        # Get new post from the user
+        new_post = request.get_json()
+
+        # Validate new post content
+        if not validate_post_data(new_post):
+            return jsonify({"error": "Fields 'title' and 'content' can't be empty or missing!"}), 400
+
+        if not validate_post_title(new_post):
+            return jsonify({"error": "Field 'title' can't be empty or missing!"}), 400
+
+        if not validate_post_content(new_post):
+            return jsonify({"error": "Field 'content' can't be empty or missing!"}), 400
+
+        # Generate new ID for the new post
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+
+        # Add new post
+        POSTS.append(new_post)
+
+        return jsonify(new_post), 201
+
+    else:
+        # Handle the GET request
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
